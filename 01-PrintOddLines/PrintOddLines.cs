154621051e1b38505690190f9ed194d4e58ac623
gianyy <gianyy@abv.bg>
2013-02-15 22:07:34 +0200

    Lines from Text

---
+++ 01-PrintOddLines/PrintOddLines.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.IO;
+using System.Text;
+namespace _01_PrintOddLines
+{
+    class PrintOddLines
+    {
+        //Just a comment ...
+        static void Main()
+        {
+            StreamReader Gireader = new StreamReader(@"../../../folktale.txt");
+            using (Gireader)
+            {
+                int number = 1;
+                string Line = Gireader.ReadLine();
+                while (Line != null)
+                {
+                    number++;
+                    if (number % 2 != 0)
+                    {
+                        Console.WriteLine(Line);
+                    }
+                    Line = Gireader.ReadLine();
+                }
+            }
+        }
+    }
+}
+
